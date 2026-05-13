f6b9ea5c6d70a8a8dc484c95169597d25fbdbf3c
Wile <proveedor-wtinoco@inteligogroup.com>
2026-05-12 20:09:04 -0500

    fixing

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -14,6 +14,8 @@ use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Assets\Css;
 use Filament\Support\Colors\Color;
+use Filament\Support\Facades\FilamentView;
+use Filament\View\PanelsRenderHook;
 use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
@@ -21,9 +23,36 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Illuminate\Support\Facades\Blade;
 
 class AdminPanelProvider extends PanelProvider
 {
+    public function boot(): void
+    {
+        FilamentView::registerRenderHook(
+            PanelsRenderHook::HEAD_END,
+            fn (): string => Blade::render('
+                <link rel="apple-touch-icon" sizes="57x57" href="{{ asset("apple-icon-57x57.png") }}">
+                <link rel="apple-touch-icon" sizes="60x60" href="{{ asset("apple-icon-60x60.png") }}">
+                <link rel="apple-touch-icon" sizes="72x72" href="{{ asset("apple-icon-72x72.png") }}">
+                <link rel="apple-touch-icon" sizes="76x76" href="{{ asset("apple-icon-76x76.png") }}">
+                <link rel="apple-touch-icon" sizes="114x114" href="{{ asset("apple-icon-114x114.png") }}">
+                <link rel="apple-touch-icon" sizes="120x120" href="{{ asset("apple-icon-120x120.png") }}">
+                <link rel="apple-touch-icon" sizes="144x144" href="{{ asset("apple-icon-144x144.png") }}">
+                <link rel="apple-touch-icon" sizes="152x152" href="{{ asset("apple-icon-152x152.png") }}">
+                <link rel="apple-touch-icon" sizes="180x180" href="{{ asset("apple-icon-180x180.png") }}">
+                <link rel="icon" type="image/png" sizes="192x192" href="{{ asset("android-icon-192x192.png") }}">
+                <link rel="icon" type="image/png" sizes="32x32" href="{{ asset("favicon-32x32.png") }}">
+                <link rel="icon" type="image/png" sizes="96x96" href="{{ asset("favicon-96x96.png") }}">
+                <link rel="icon" type="image/png" sizes="16x16" href="{{ asset("favicon-16x16.png") }}">
+                <link rel="manifest" href="{{ asset("manifest.json") }}">
+                <meta name="msapplication-TileColor" content="#ffffff">
+                <meta name="msapplication-TileImage" content="{{ asset("ms-icon-144x144.png") }}">
+                <meta name="theme-color" content="#ffffff">
+            ')
+        );
+    }
+
     public function panel(Panel $panel): Panel
     {
         return $panel
@@ -31,6 +60,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->favicon(asset('favicon.ico'))
             ->colors([
                 'primary' => Color::Amber,
             ])
