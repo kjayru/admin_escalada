a3981c814b372af89a1c2703272012608a9b03c9
Wile <proveedor-wtinoco@inteligogroup.com>
2026-05-13 20:26:45 -0500

    fix

--- app/Http/Controllers/Api/V1/PayPalController.php
+++ app/Http/Controllers/Api/V1/PayPalController.php
@@ -141,8 +141,8 @@ class PayPalController extends Controller
 
         try {
             $response = Http::withToken($token)
-                ->withHeaders(['Content-Type' => 'application/json'])
-                ->post("{$this->baseUrl}/v2/checkout/orders/{$orderId}/capture", []);
+                ->withBody('{}', 'application/json')
+                ->post("{$this->baseUrl}/v2/checkout/orders/{$orderId}/capture");
 
             if ($response->successful()) {
                 $data = $response->json();
