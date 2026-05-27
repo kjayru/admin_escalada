ea066d16de1e717fe969ca27204c29bcca2aad4b
Wile Tinoco <wiltinoco@gmail.com>
2026-05-26 23:32:23 -0500

    fix

--- app/Filament/Resources/MemberResource/Pages/CreateMember.php
+++ app/Filament/Resources/MemberResource/Pages/CreateMember.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateMember extends CreateRecord
 {
     protected static string $resource = MemberResource::class;
+
+    protected function afterCreate(): void
+    {
+        $this->redirect($this->getResource()::getUrl('create'));
+    }
 }
