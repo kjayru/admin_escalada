29010b438acf9f160b21af165dbbf644d153284b
Wile Tinoco <wiltinoco@gmail.com>
2026-05-26 22:40:23 -0500

    fix

--- app/Filament/Resources/SponsorPlacementResource.php
+++ app/Filament/Resources/SponsorPlacementResource.php
@@ -43,6 +43,8 @@ class SponsorPlacementResource extends Resource
 
     protected static ?int $navigationSort = 2;
 
+    protected static bool $shouldRegisterNavigation = false;
+
     public static function form(Schema $schema): Schema
     {
         return $schema
